df003857f0d697170d12a1a04fc363737b9195c8
Jakub Olcha <olo75jg12@gmail.com>
2024-08-16 11:46:19 +0200

    up

--- components/header/Header.tsx
+++ components/header/Header.tsx
@@ -2,37 +2,26 @@ import React from "react";
 import TopBar from "./topBar/TopBar";
 import styles from "./Header.module.css";
 import HorizontalCarousel from "../horizontalCarousel/HorizontalCarousel";
-import RevealTextOnScroll from "@/animations/revealTextOnScroll/RevealTextOnScroll";
 
 const Header = () => {
   return (
     <div className={`${styles.container}`}>
       <TopBar />
-      <div className={`${styles.imageAndCarouselWrapper} px-main-left`}>
-        <div className={styles.imageWrapper}>
-          {/* <Image src={Picture} alt="picture" /> */}
-        </div>
-        <div className={`${styles.carouselWrapper} typo-2xl`}>
-          <HorizontalCarousel speed={10} direction="right">
-            <div> Frontend </div>
-          </HorizontalCarousel>
-
-          <HorizontalCarousel speed={8} direction="left">
-            <div> Developer </div>
-          </HorizontalCarousel>
+      <div>
+        <div>FRONTEND</div>
+        <div>
+          <p>
+            Crafting visually stunning and highly interactive web experiences
+            with a blend of creativity and technical expertise.
+          </p>
         </div>
       </div>
+      <HorizontalCarousel speed={1} direction="right">
+        <div>JAKUB OLCHA</div>
+      </HorizontalCarousel>
+      <div>DEVELOPER</div>
       <div className={`${styles.bottomRowTexts} px-main`}>
-        <div className={`typo-xl ${styles.titleWrapper}`}>
-          <h1>FRONTEND</h1>
-          <h1>DEVELOPER</h1>
-        </div>
-        <div className={`typo-md ${styles.descriptionWrapper}`}>
-          <RevealTextOnScroll
-            text="Crafting visually stunning and highly interactive web experiences
-              with a blend of creativity and technical expertise."
-          />
-        </div>
+        <div className={`typo-md ${styles.descriptionWrapper}`}></div>
       </div>
     </div>
   );
