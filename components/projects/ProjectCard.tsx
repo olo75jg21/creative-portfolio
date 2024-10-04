1c27d58de86cfdcfe520942b69aa7122e6470799
Jakub Olcha <olo75jg12@gmail.com>
2024-10-04 09:59:39 +0200

    projects

--- components/projects/ProjectCard.tsx
+++ components/projects/ProjectCard.tsx
@@ -1,9 +1,28 @@
 import React from "react";
+import styles from "./ProjectCard.module.scss";
+import { Project } from "@/types";
 
-type Props = {};
+type Props = {
+  project: Project;
+};
 
-const ProjectCard = (props: Props) => {
-  return <div>ProjectCard</div>;
+const ProjectCard = ({ project }: Props) => {
+  return (
+    <div className={styles.cardContainer}>
+      {/* <img src={project.thumbnail} alt={project.title} className={styles.thumbnail} /> */}
+      <div className={styles.card}>
+        <h2 className={styles.projectTitle}>{project.title}</h2>
+        <p className={styles.projectDescription}>{project.description}</p>
+        <div className={styles.technologies}>
+          {project.technologies.map((tech, index) => (
+            <span key={index} className={styles.technology}>
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ProjectCard;
--- components/projects/Projects.tsx
+++ components/projects/Projects.tsx
@@ -1,26 +1,14 @@
 import React from "react";
 import styles from "./Projects.module.css";
 import { projectsData } from "@/data";
+import ProjectCard from "./ProjectCard";
 
 const Projects = () => {
   return (
     <div>
       <div className={styles.projectsContainer}>
         {projectsData.map((project) => (
-          <div key={project.id} className={styles.projectCard}>
-            {/* <img src={project.thumbnail} alt={project.title} className={styles.thumbnail} /> */}
-            <div className={styles.cardContent}>
-              <h2 className={styles.projectTitle}>{project.title}</h2>
-              <p className={styles.projectDescription}>{project.description}</p>
-              <div className={styles.technologies}>
-                {project.technologies.map((tech, index) => (
-                  <span key={index} className={styles.technology}>
-                    {tech}
-                  </span>
-                ))}
-              </div>
-            </div>
-          </div>
+          <ProjectCard key={project.id} project={project} />
         ))}
       </div>
     </div>
